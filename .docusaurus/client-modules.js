1a0d09a803712469801d95655a4a792f28857a0a
gaebi4102 <129516516+gaebi4102@users.noreply.github.com>
2023-03-31 19:45:07 +0200

    .

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/gianlucaaebi/git/m231/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/gianlucaaebi/git/m231/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/gianlucaaebi/git/m231/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/gianlucaaebi/git/m231/src/css/custom.css'),
+];
---
+++ .docusaurus/routes.js
@@ -0,0 +1,220 @@
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+
+export default [
+  {
+    path: '/bbzbl-modul-m231/__docusaurus/debug',
+    component: ComponentCreator('/bbzbl-modul-m231/__docusaurus/debug', '6ed'),
+    exact: true
+  },
+  {
+    path: '/bbzbl-modul-m231/__docusaurus/debug/config',
+    component: ComponentCreator('/bbzbl-modul-m231/__docusaurus/debug/config', 'dfb'),
+    exact: true
+  },
+  {
+    path: '/bbzbl-modul-m231/__docusaurus/debug/content',
+    component: ComponentCreator('/bbzbl-modul-m231/__docusaurus/debug/content', '574'),
+    exact: true
+  },
+  {
+    path: '/bbzbl-modul-m231/__docusaurus/debug/globalData',
+    component: ComponentCreator('/bbzbl-modul-m231/__docusaurus/debug/globalData', '34a'),
+    exact: true
+  },
+  {
+    path: '/bbzbl-modul-m231/__docusaurus/debug/metadata',
+    component: ComponentCreator('/bbzbl-modul-m231/__docusaurus/debug/metadata', '76c'),
+    exact: true
+  },
+  {
+    path: '/bbzbl-modul-m231/__docusaurus/debug/registry',
+    component: ComponentCreator('/bbzbl-modul-m231/__docusaurus/debug/registry', '3a8'),
+    exact: true
+  },
+  {
+    path: '/bbzbl-modul-m231/__docusaurus/debug/routes',
+    component: ComponentCreator('/bbzbl-modul-m231/__docusaurus/debug/routes', '495'),
+    exact: true
+  },
+  {
+    path: '/bbzbl-modul-m231/markdown-page',
+    component: ComponentCreator('/bbzbl-modul-m231/markdown-page', 'b94'),
+    exact: true
+  },
+  {
+    path: '/bbzbl-modul-m231/search',
+    component: ComponentCreator('/bbzbl-modul-m231/search', 'd90'),
+    exact: true
+  },
+  {
+    path: '/bbzbl-modul-m231/slides',
+    component: ComponentCreator('/bbzbl-modul-m231/slides', '23e'),
+    exact: true
+  },
+  {
+    path: '/bbzbl-modul-m231/docs',
+    component: ComponentCreator('/bbzbl-modul-m231/docs', '395'),
+    routes: [
+      {
+        path: '/bbzbl-modul-m231/docs/',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/', '210'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/beurteilungen/lb1',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/beurteilungen/lb1', '23f'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/beurteilungen/lb2',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/beurteilungen/lb2', '99d'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/beurteilungen/lb3',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/beurteilungen/lb3', 'e79'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/category/beurteilungen',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/category/beurteilungen', '7e7'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/category/datenschutz',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/category/datenschutz', 'b11'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/category/datensicherheit',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/category/datensicherheit', '0d6'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/category/frameworks',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/category/frameworks', '308'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/category/identity-und-access-management',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/category/identity-und-access-management', '29e'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/Datenschutz/Daten',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/Datenschutz/Daten', 'daa'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/Datenschutz/EU',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/Datenschutz/EU', 'fc6'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/Datenschutz/Schweiz',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/Datenschutz/Schweiz', '7ca'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/Datensicherheit/Gefahren',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/Datensicherheit/Gefahren', '711'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/Frameworks/Nist',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/Frameworks/Nist', '81b'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/Identity und Access Management/Identity',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/Identity und Access Management/Identity', '1c3'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/lektionen/',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/lektionen/', 'c8a'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/lektionen/woche-01',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/lektionen/woche-01', '231'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/lektionen/woche-02',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/lektionen/woche-02', '53e'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/lektionen/woche-03',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/lektionen/woche-03', 'd6d'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/lektionen/woche-04',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/lektionen/woche-04', '1e4'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/lektionen/woche-05',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/lektionen/woche-05', '507'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/lektionen/woche-06',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/lektionen/woche-06', '2d8'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/lektionen/woche-07',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/lektionen/woche-07', 'b65'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/lektionen/woche-08',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/lektionen/woche-08', '333'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/bbzbl-modul-m231/docs/lektionen/woche-09',
+        component: ComponentCreator('/bbzbl-modul-m231/docs/lektionen/woche-09', '627'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      }
+    ]
+  },
+  {
+    path: '/bbzbl-modul-m231/',
+    component: ComponentCreator('/bbzbl-modul-m231/', 'fe9'),
+    exact: true
+  },
+  {
+    path: '*',
+    component: ComponentCreator('*'),
+  },
+];
